8c028e558207deeb7c8e1d10e2f91b759285b473
ajtsnowflaketesting <123494759+ajtsnowflaketesting@users.noreply.github.com>
2023-01-24 14:03:39 -0500

    Update streamlit_app.py
    New Build Your Own Fruit Smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,6 @@ streamlit.header ('Breakfast Menu')
 streamlit.text (':-) Omega 3 & Blueberry oatmeal')
 streamlit.text ('@#Kale, Spinach & Rocket Smoothie ')
 streamlit.text ('8-9 Hard-boiled Free-Ranch Egg')
+streamlit.text ('Avocado Toast')
 
-
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
