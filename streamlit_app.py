9f1f66396c021a6732c7cd27d03304de94f18510
ajtsnowflaketesting <123494759+ajtsnowflaketesting@users.noreply.github.com>
2023-01-24 13:58:49 -0500

    Update streamlit_app.py
    Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 streamlit.title ('My Parents New Healthy Diner')
 
 streamlit.header ('Breakfast Menu')
-streamlit.text ('Omega 3 & Blueberry oatmeal')
-streamlit.text ('Kale, Spinach & Rocket Smoothie ')
-streamlit.text ('Hard-boiled Free-Ranch Egg')
+streamlit.text (':-) Omega 3 & Blueberry oatmeal')
+streamlit.text ('@#Kale, Spinach & Rocket Smoothie ')
+streamlit.text ('8-9 Hard-boiled Free-Ranch Egg')
 
 
