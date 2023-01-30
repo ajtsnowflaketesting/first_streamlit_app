f7b2ab4e8780fc058b3929ad7020f5e99135427f
ajtsnowflaketesting <123494759+ajtsnowflaketesting@users.noreply.github.com>
2023-01-30 12:51:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,9 +21,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 # Fruit values put into columns 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Display table with fruit values in column
+# Display table with fruit advice in column
 streamlit.dataframe(fruityvice_normalized)
