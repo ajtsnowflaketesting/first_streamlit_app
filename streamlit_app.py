eecc681a7f1b4d1aa51cc54a78cd9ab556ee864e
ajtsnowflaketesting <123494759+ajtsnowflaketesting@users.noreply.github.com>
2023-01-30 16:48:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Display table with fruit advice response normalized in column values.
 streamlit.dataframe(fruityvice_normalized)
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered this fruit:****-- ', fruit_choice)
+streamlit.write('The user entered this fruit:&', fruit_choice)
 
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
