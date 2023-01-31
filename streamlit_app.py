5459955b180ee965bad34e8165868313d1d0fcd0
ajtsnowflaketesting <123494759+ajtsnowflaketesting@users.noreply.github.com>
2023-01-30 22:43:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ except URLError as e:
            return my_cur.fetchall()
 
   #Add a button   
-  if streamlit.button("Get Fruit Load List")
+  if streamlit.button("Get Fruit Load List"):
       my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
       my_data_rows = get_fruit_load_list()
       streamlit.dataframe(my_data_rows)
@@ -62,7 +62,7 @@ except URLError as e:
            return "Thanks for adding " + new_fruit
 
   add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-  if streamlit.button("Add a fruit to the list")
+  if streamlit.button("Add a fruit to the list"):
       my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
       back_from_function =insert_row_snowflake (add_my_fruit) 
       streamlit.text(back_from_function)
