697aba4dc0f6a4261bf29a1f4ea2bbb037c30f0f
ajtsnowflaketesting <123494759+ajtsnowflaketesting@users.noreply.github.com>
2023-01-30 20:57:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
-
+import requests
+import snowflake.connector
+from urllib.error import URLError
 streamlit.title ('My Parents New Healthy Diner')
 
 streamlit.header ('Breakfast Menu')
@@ -19,7 +21,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 # Get Fruit values response normalizaed and stored  into columns 
